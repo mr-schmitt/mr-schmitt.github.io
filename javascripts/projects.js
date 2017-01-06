5a155f897fc38fbf09e8f78d187f2e3ed43d6aab
Michael Schmitt <schm177y@gmail.com>
2017-01-06 15:45:19 +0000

    moved project info to projects.js file and created a function to generate projects table

--- javascripts/projects.js
+++ javascripts/projects.js
@@ -1,19 +1,51 @@
-var projects = {
-     
-     gd1 : {
-        name : "Game Development 1 - Example Student Project",
+// TODO - add an object to hold all projects
+// TODO - add some basic error handling for table creation
+// TODO - createTable function should create all tables from parent project object
+
+
+
+var gd1 = {
+        title : "Game Development 1 - Example Student Project",
         tags : ["Unity", "C#", "Curriculum"],
         text : "Example final project for the Youth Digital <a href='http://www.youthdigital.com/3d-game-development-1'>Game Development 1</a> course.",
         icon : "img/ud164.png",
         id : "gd1",
         link: "https://mr-schmitt.github.io/UD1WebGLBuild/"
-    }
-    
 };
 
-// Need to add way to access this script from index.html?
+var ballGame = {
+    title : "RollerBall Game",
+    tags : ["Unity", "C#"],
+    text : "Simple game built with Unity",
+    icon : "img/ball64.png",
+    id : "ball-game",
+    link: "https://mr-schmitt.github.io/ball-game"
+};
+
+var molarSystem = {
+    title : "The Molar System",
+    tags : ["Unity", "C#"],
+    text : "Mostly accurate interactive Solar System replica",
+    icon : "img/molar64.png",
+    id: "molar-system",
+    link : "https://mr-schmitt.github.io/molar-system"
+};
+
+var pong = {
+    title : "Two-Player Pong Game",
+    tags : ["JS"],
+    text : "In-browser game built using Superpowers",
+    icon : "img/pong64.png",
+    id: "pong",
+    link : "https://mr-schmitt.github.io/pong-sp"
+};
+
 function createTable(proj){
     // add all HTML necessary for project table
-    document.write("<table><tr><td><a href =" + proj.link + "><img src=" + proj.icon +"></a></td><td><h3><a id="+ proj.id + "class='anchor' href='#" + proj.id + " aria-hidden='true'><span aria-hidden='true' class='octicon octicon-link'></span></a>" + proj.title + "</h3><p>" + proj.text + "</p></td></tr><tr></table>");
-    
+    document.getElementById("projects").insertAdjacentHTML("beforeEnd", "<tr><td><a href ='" + proj.link + "'><img src='" + proj.icon +"'></a></td><td><h3><a id='"+ proj.id + "' class='anchor' href='#" + proj.id + "' aria-hidden='true'><span aria-hidden='true' class='octicon octicon-link'></span></a>" + proj.title + "</h3><p>" + proj.text + "</p></td></tr>");
 }
+
+createTable(gd1);
+createTable(ballGame);
+createTable(molarSystem);
+createTable(pong);
