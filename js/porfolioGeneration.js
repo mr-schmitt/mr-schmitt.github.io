54b13e2a01b7e330fec1090cb7bbacf817de28e1
Michael Schmitt <schm177y@gmail.com>
2018-08-29 16:32:38 +0000

    Added DFT entry and removed personal music from portfolio

--- js/porfolioGeneration.js
+++ js/porfolioGeneration.js
@@ -1,5 +1,6 @@
 var projectList = [
         {title : "Mod Design: Dimensions", year : "2016", tag : ["code", "design", "art"], text : "An online course in Java Programming and Minecraft Modding", icon : "img/MD1D2.png", link : "http://www.youthdigital.com"},
+        {title : "Duncan's Flying Tugboat", year : "2018", tag : ["code", "art"], text : "An interactive storybook coming this fall for iOS", icon : "img/DFT.png", link: "https://www.skyfarminteractive.com/"},
         {title : "Snailboat", year : "2017", tag : ["code", "design", "audio"], text : "Set snail for adventure! A challenging grid-based puzzle game.", icon : "img/snailboat.png", link: "https://toomuchtomato.itch.io/snailboat"},
         {title : "Game Development 1", year : "2016", tag : ["code", "design"], text : "An online course in Unity 3D Game development and C# programming", icon : "img/UD1.png", link: "http://www.youthdigital.com/3d-game-development-1"},
         {title : "Chicken Tender", year : "2017", tag : ["code", "design","audio", "art"], text : "Tend to your flock! A silly agricultural simulator.", icon : "img/chickentender.png", link: "https://toomuchtomato.itch.io/chicken-tender"},
@@ -9,8 +10,8 @@ var projectList = [
         {title : "Bowel MOOvement", year : "2018", tag : ["code", "design", "audio", "art"], text : "Navigate tricky platforming challenges in this artsy web game.", icon : "img/bm-portfolio.png", link: "https://toomuchtomato.itch.io/bowel-moovement"},
         {title : "Smallworld", year : "2017", tag : ["code", "design", "audio", "art"], text : "A game that exists purely for a funky soundtrack and silly characters.", icon : "img/smallworld.png", link: "https://toomuchtomato.itch.io/smallworld"},
         {title : "Entropy", year : "2017", tag : ["code", "design", "audio", "art"], text : "A short adventure/puzzle game with fun music and philosophical oddball characters", icon : "img/entropy.png", link: "https://toomuchtomato.itch.io/entropy"},
-        {title : "Art & Design Portfolio", year : "2018", tag : ["art"], text : "A gallery of Michael's illustration, animation, and design work", icon: "img/comic.png", link : "https://imgur.com/a/uIJ7V"},
-        {title : "Mr.Stripey", year : "2013-2018", tag : ["audio"], text : "A collection of music composed, recorded, and or produced by Michael", icon: "img/mrs.png", link : "https://misterstripey.bandcamp.com"}
+        {title : "Art & Design Portfolio", year : "2018", tag : ["art"], text : "A gallery of Michael's illustration, animation, and design work", icon: "img/comic.png", link : "https://imgur.com/a/uIJ7V"}
+        //{title : "Mr.Stripey", year : "2013-2018", tag : ["audio"], text : "A collection of music composed, recorded, and or produced by Michael", icon: "img/mrs.png", link : "https://misterstripey.bandcamp.com"}
 
         // images should be 400w x 300h
         
