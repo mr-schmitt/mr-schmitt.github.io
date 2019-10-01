1d69d6136a102365c240cb2833e6fbfc46a4d23d
mr-schmitt <schmitt.michael.r@gmail.com>
2019-10-01 17:21:39 -0400

    Fix broken YD links

--- js/porfolioGeneration.js
+++ js/porfolioGeneration.js
@@ -1,20 +1,20 @@
 var projectList = [
-        {title : "Mod Design: Dimensions", year : "2016", tag : ["code", "design", "art"], text : "An online course in Java Programming and Minecraft Modding", icon : "img/MD1D2.png", link : "http://www.youthdigital.com"},
-        {title : "Duncan's Flying Tugboat", year : "2018", tag : ["code", "art"], text : "An interactive storybook coming this fall for iOS", icon : "img/DFT.png", link: "https://www.skyfarminteractive.com/"},
-        {title : "Snailboat", year : "2017", tag : ["code", "design", "audio"], text : "Set snail for adventure! A challenging grid-based puzzle game.", icon : "img/snailboat.png", link: "https://toomuchtomato.itch.io/snailboat"},
-        {title : "Game Development 1", year : "2016", tag : ["code", "design"], text : "An online course in Unity 3D Game development and C# programming", icon : "img/UD1.png", link: "http://www.youthdigital.com/3d-game-development-1"},
+        {title : "Mod Design: Dimensions", year : "2016", tag : ["code", "design", "art"], text : "An online course in Java Programming and Minecraft Modding", icon : "img/MD1D2.png", link : "https://www.apexlearning.com/technology-courses"},
+        {title : "Duncan's Flying Tugboat", year : "2019", tag : ["code", "art"], text : "An interactive storybook coming this soon for iOS", icon : "img/DFT.png", link: "https://www.skyfarminteractive.com/"},
+        {title : "Snailboat Mobile", year : "2019", tag : ["code", "design", "audio"], text : "A short and sweet puzzle game for Android and iOS", icon : "img/snailboat-mockups.png", link: "https://play.google.com/store/apps/details?id=com.TooMuchTomato.snailboat_android"},
+        {title : "Game Development 1", year : "2016", tag : ["code", "design"], text : "An online course in Unity 3D Game development and C# programming", icon : "img/UD1.png", link: "https://www.apexlearning.com/technology-courses"},
         {title : "Chicken Tender", year : "2017", tag : ["code", "design","audio", "art"], text : "Tend to your flock! A silly agricultural simulator.", icon : "img/chickentender.png", link: "https://toomuchtomato.itch.io/chicken-tender"},
-        {title : "Server Design 1", year : "2015", tag : ["code", "design"], text : "An online course in Java Programming. Learn to create your own Minecraft Server", icon : "img/SD1.png", link : "http://www.youthdigital.com/server-design-1"},
+        {title : "Server Design 1", year : "2015", tag : ["code", "design"], text : "An online course in Java Programming. Learn to create your own Minecraft Server", icon : "img/SD1.png", link : "https://www.apexlearning.com/technology-courses"},
         {title : "The Machine's Garden", year : "2018", tag : ["code", "design", "audio"], text : "Currently in Development. Use the intelligence of a decommissioned military supercomputer to solve puzzles and guide the lives of humble villagers", icon : "img/tmg.png", link: "https://twitter.com/machinesgarden"},
         {title : "Ball's Revenge", year : "2017", tag : ["code"], text : "A quirky arcade-style game for Android and iOS about an alien named Ball", icon : "img/ballsrevenge.png", link: "https://play.google.com/store/apps/details?id=com.Suraksham.BallsRevengeLite&hl=en_US"},
         {title : "Bowel MOOvement", year : "2018", tag : ["code", "design", "audio", "art"], text : "Navigate tricky platforming challenges in this artsy web game.", icon : "img/bm-portfolio.png", link: "https://toomuchtomato.itch.io/bowel-moovement"},
-        {title : "Smallworld", year : "2017", tag : ["code", "design", "audio", "art"], text : "A game that exists purely for a funky soundtrack and silly characters.", icon : "img/smallworld.png", link: "https://toomuchtomato.itch.io/smallworld"},
-        {title : "Entropy", year : "2017", tag : ["code", "design", "audio", "art"], text : "A short adventure/puzzle game with fun music and philosophical oddball characters", icon : "img/entropy.png", link: "https://toomuchtomato.itch.io/entropy"},
-        {title : "Art & Design Portfolio", year : "2018", tag : ["art"], text : "A gallery of Michael's illustration, animation, and design work", icon: "img/comic.png", link : "https://imgur.com/a/uIJ7V"}
+        {title : "Snailboat", year : "2017", tag : ["code", "design", "audio"], text : "Set snail for adventure! A challenging grid-based puzzle game.", icon : "img/snailboat.png", link: "https://toomuchtomato.itch.io/snailboat"},
+        {title : "Entropy", year : "2017", tag : ["code", "design", "audio", "art"], text : "A short adventure/puzzle game with fun music and philosophical oddball characters", icon : "img/entropy.png", link: "https://toomuchtomato.itch.io/entropy"}
+        //{title : "Art & Design Portfolio", year : "2018", tag : ["art"], text : "A gallery of Michael's illustration, animation, and design work", icon: "img/comic.png", link : "https://imgur.com/a/uIJ7V"}
         //{title : "Mr.Stripey", year : "2013-2018", tag : ["audio"], text : "A collection of music composed, recorded, and or produced by Michael", icon: "img/mrs.png", link : "https://misterstripey.bandcamp.com"}
 
         // images should be 400w x 300h
-        
+
         // defunct projects
         //{title : "Professions - EP", year : "2016", tag : ["audio"], text : "An EP of experimental music and covers", icon : "img/professions.png",  link : "https://misterstripey.bandcamp.com/album/professions"}
         //{title : "Hello Michael!", year : "2014", tag : ["design"], text : "A blog about my experiences teaching English in rural Japan", icon : "img/hellomichael.png",  link : "http://hellomaikeru.blogspot.com/"},
@@ -26,10 +26,10 @@ var projectList = [
         //{title : "Two-Player Pong Game", year : "2016", tags : ["JS"], text : "In-browser game built using Superpowers", icon : "img/pong64.png", id: "pong", link : "https://mr-schmitt.github.io/pong-sp"},
         //{title : "Gardening - EP", year : "2017", tag : ["audio"], text : "An EP of experimental music", icon : "img/gardening.png",  link : "https://misterstripey.bandcamp.com/album/gardening"},
         //{title : "Hole - EP", year : "2015", tag : ["audio"], text : "An EP of electronic music", icon : "img/hole.png",  link : "https://misterstripey.bandcamp.com/album/hole"}
-        
+
     ];
-    
-    
+
+
 /* global generateTables */
 function generateProjectEntries(projArray){
     document.getElementById("projects").innerHTML= "";
@@ -38,7 +38,7 @@ function generateProjectEntries(projArray){
             console.log("Could not find data for project " + projArray[i] + ".  Skipping table entry.");
         } else {
             // add all HTML necessary for project entry
-            document.getElementById("projects").insertAdjacentHTML("beforeEnd", 
+            document.getElementById("projects").insertAdjacentHTML("beforeEnd",
                 "<figure class='white'><a href='" + projArray[i].link + "'>" +
                 "<img src='" + projArray[i].icon + "' />" +
                 "<dl><dt>" + projArray[i].title +"  ("  +  projArray[i].year + ")</dt>" +
